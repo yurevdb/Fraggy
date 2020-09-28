ca92e750721a754de0ceb0fbca56dcef821a287d
Yuré Vanderbruggen <yurevdb@gmail.com>
2020-09-28 21:47:22 +0200

    fancy message

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// The prefix for the bot
 const prefix = "!frag"
 
-client.on("ready", () => {
-  console.log(`Fraggy is here! We've got ${client.users.size} users, in ${client.channels.size} channels.`);
-});
-
 client.on("message", async message => {
-  
+  // Check if bot is the author
   if(message.author.bot) return
 
+  // Check if the message contains the prefix
   if(!message.content.startsWith(prefix)) return
 
+  // Filter out the prefix for the message
   const withoutPrefix = message.content.slice(prefix.length);
 
-  message.channel.send(withoutPrefix).then(answer => answer.react('760218905808863293'));
+  const split = withoutPrefix.split(/ +/);
+
+  const command = split[0];
+  const args = split.slice(1);
+
+  switch(command) {
+    case "role":
+      const embed = new MessageEmbed()
+        .setTitle('Get your own role')
+        .setColor(0xff0000)
+        .setDescription('React to the role you want to get!');
+      message.channel.send(embed).then(answer => answer.react('760218905808863293'));
+  }
 });
 
+// Log the client in
 client.login(process.env.token);
