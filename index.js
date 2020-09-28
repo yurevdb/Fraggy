210545f3ece855e4c28ed53bb26932c848b4f7ec
Yuré Vanderbruggen <yurevdb@gmail.com>
2020-09-28 20:29:44 +0200

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const prefix = "!frag"
+
+client.on("ready", () => {
+  console.log("Fraggy is here! We've got ${client.users.size} users, in ${client.channels.size} channels.");
+});
+
+client.on("message", async message => {
+  
+  if(message.author.bot) return
+
+  if(!message.content.startswith(prefix)) return
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  message.channel.send(args.join(" "));
+
+});
