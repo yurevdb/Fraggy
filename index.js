336bc4e131dde2ea9ebce8f0de51d6192d657544
Yuré Vanderbruggen <yurevdb@gmail.com>
2020-09-28 22:01:49 +0200

    sup

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ client.on("message", async message => {
   console.log(command)
   switch(command) {
     case "role":
-      const embed = new Discord.MessageEmbed()
+      const embed = new MessageEmbed()
         .setTitle('Get your own role')
         .setColor(0xff0000)
         .setDescription('React to the role you want to get!');
       message.channel.send(embed).then(answer => answer.react('760218905808863293'));
     default: 
-      message.channel.send(`Type ${prefix} help to see the availlable commands`);
+      message.channel.send(`Type "${prefix} help" to see the availlable commands`);
   }
 });
 
